2226e6b73890de8f4ac78994db19273b4f3d3bef
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-01 21:41:22 +0200

    Rename test module

---
+++ test/quickledger/test/nordea-parser.clj
@@ -0,0 +1,14 @@
+(ns quickledger.test.nordea-parser
+  (:use [quickledger.nordea-parser])
+  (:use [clojure.test]))
+
+(deftest read-nordea-csv-test
+  (is (= (read-nordea-csv "testdata/test.csv" "Assets:Checking:Nordea")
+         (list {:date "2011-08-26"
+                :desc "Advice dog"
+                :amount (list 'debit 400.00)
+                :account "Assets:Checking:Nordea"}
+               {:date "2011-08-26"
+                :desc "Lolcats"
+                :amount (list 'debit 79.00)
+                :account "Assets:Checking:Nordea"} ))))
