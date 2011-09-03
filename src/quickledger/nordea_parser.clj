1b04dfc6fcebefe8a6bd8e0f2a3a70b809734c12
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-03 17:52:17 +0200

    Change amount format

--- src/quickledger/nordea_parser.clj
+++ src/quickledger/nordea_parser.clj
@@ -2,16 +2,11 @@
 
 (require 'clojure-csv.core)
 
-(defn amount-from-account [amount]
-  (if (< amount 0)
-    (list 'debit (- amount))
-    (list 'credit amount)))
-
 (defn filter-nordea-csv [csvline account]
   {:date (nth csvline 0)
    :desc (nth csvline 1)
-   :amount (amount-from-account (Float/parseFloat (nth csvline 3)))
-   :account account})
+   :transfers [{:amount (Float/parseFloat (nth csvline 3))
+                :account account}]})
 
 (defn read-nordea-csv [filename account]
   (map (fn [csvline] (filter-nordea-csv csvline account))
--- test/quickledger/test/nordea_parser.clj
+++ test/quickledger/test/nordea_parser.clj
@@ -4,11 +4,11 @@
 
 (deftest read-nordea-csv-test
   (is (= (read-nordea-csv "testdata/test.csv" "Assets:Checking:Nordea")
-         (list {:date "2011-08-26"
-                :desc "Advice dog"
-                :amount (list 'debit 400.00)
-                :account "Assets:Checking:Nordea"}
-               {:date "2011-08-26"
-                :desc "Lolcats"
-                :amount (list 'debit 79.00)
-                :account "Assets:Checking:Nordea"} ))))
+         [{:date "2011-08-26"
+           :desc "Advice dog"
+           :transfers [{:amount -400.00
+                        :account "Assets:Checking:Nordea"}]}
+          {:date "2011-08-26"
+           :desc "Lolcats"
+           :transfers [{:amount -79.00
+                        :account "Assets:Checking:Nordea"}]}])))
