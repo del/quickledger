b24a097977a6354b341acb29fa7194731726acbd
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-05 22:59:38 +0200

    Add file of test filters

---
+++ testdata/testfilters.clj
@@ -0,0 +1,16 @@
+[{:certain true
+  :regex #"cats"
+  :transfers [{:account "Expenses:Lolcats"
+               :amount 0.8}
+              {:account "Expenses:Vat"
+               :amount 0.2}]}
+ {:certain false
+  :regex #"dogs"
+  :transfers [{:account "Expenses:Raccoons"
+               :amount 0.5}
+              {:account "Expenses:Toiletries"
+               :amount 0.5}]}
+ {:certain false
+  :regex #"dogs"
+  :transfers [{:account "Expenses:Goats"
+               :amount 1.0}]}]
