1c30e1817bc970372e600f28194e8ddb4a40196f
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-06 15:50:12 +0200

    Update test data for harmony in tests

--- test/quickledger/test/nordea_parser.clj
+++ test/quickledger/test/nordea_parser.clj
@@ -2,13 +2,13 @@
   (:use [quickledger.nordea-parser])
   (:use [clojure.test]))
 
-(deftest read-nordea-csv-test
-  (is (= (read-nordea-csv "testdata/test.csv" "Assets:Checking:Nordea")
-         [{:date "2011-08-26"
+(deftest read-csv-test
+  (is (= (read-csv "testdata/test.csv" "Assets:Checking:Nordea")
+         [{:date "2011-08-27"
            :desc "Advice dog"
-           :transfers [{:amount -400.00
+           :transfers [{:amount -200.00
                         :account "Assets:Checking:Nordea"}]}
           {:date "2011-08-26"
            :desc "Lolcats"
-           :transfers [{:amount -79.00
+           :transfers [{:amount -100.00
                         :account "Assets:Checking:Nordea"}]}])))
