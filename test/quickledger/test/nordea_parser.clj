d9d04f4cca178d50271ddbd66356785ca3fe5181
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-06 15:52:31 +0200

    Update test for new filename

--- test/quickledger/test/nordea_parser.clj
+++ test/quickledger/test/nordea_parser.clj
@@ -3,7 +3,7 @@
   (:use [clojure.test]))
 
 (deftest read-csv-test
-  (is (= (read-csv "testdata/test.csv" "Assets:Checking:Nordea")
+  (is (= (read-csv "testdata/testcsv.csv" "Assets:Checking:Nordea")
          [{:date "2011-08-27"
            :desc "Advice dog"
            :transfers [{:amount -200.00
