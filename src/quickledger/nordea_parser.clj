b4bc0dbcdef64e59484c2794dd03e8f111670120
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-06 15:50:34 +0200

    Rename read-nordea-csv to read-csv

--- src/quickledger/nordea_parser.clj
+++ src/quickledger/nordea_parser.clj
@@ -1,6 +1,5 @@
-(ns quickledger.nordea-parser)
-
-(require 'clojure-csv.core)
+(ns quickledger.nordea-parser
+  (:require clojure-csv.core))
 
 (defn filter-nordea-csv [csvline account]
   {:date (nth csvline 0)
@@ -8,6 +7,6 @@
    :transfers [{:amount (Float/parseFloat (nth csvline 3))
                 :account account}]})
 
-(defn read-nordea-csv [filename account]
+(defn read-csv [filename account]
   (map (fn [csvline] (filter-nordea-csv csvline account))
        (rest (clojure-csv.core/parse-csv (slurp filename)))))
