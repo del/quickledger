bb542b0f867553e0946429a1eb73561eefc8cde0
Daniel Eliasson <daniel.eliasson@klarna.com>
2011-09-08 11:24:10 +0200

    Add dependency, autocompile directives and a main directive

--- project.clj
+++ project.clj
@@ -4,5 +4,10 @@
                  [org.clojure/clojure "1.2.1"]
                  [clojure-csv/clojure-csv "1.3.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/tools.cli "0.1.0"]
                  ]
-  :dev-dependencies [[swank-clojure "1.3.2"]])
+  :dev-dependencies [[swank-clojure "1.3.2"]]
+  :aot [quickledger.ledger-output
+        quickledger.nordea-parser
+        quickledger.transaction-filter]
+  :main quickledger.core)
