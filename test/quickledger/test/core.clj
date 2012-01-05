b46b02f2ec77ced1d405fe76d94e73c1d411ec67
Daniel Eliasson <daniel.eliasson@klarna.com>
2012-01-05 10:47:10 +0100

    Fix incorrect ordering in test expected data

--- test/quickledger/test/core.clj
+++ test/quickledger/test/core.clj
@@ -7,7 +7,16 @@
   (let [filter-file "data/test/testfilters.clj"
         csv-file "data/test/testcsv.csv"
         account-name "Assets:Checking:Nordea"
-        expected [[{:date "2011-08-27"
+        expected [{:date "2011-08-26"
+                   :desc "Cat with gherkins"
+                   :certain true
+                   :transfers [{:account "Assets:Checking:Nordea"
+                                :amount -100.00}
+                               {:account "Expenses:Lolcats"
+                                :amount 80.0}
+                               {:account "Expenses:Vat"
+                                :amount 20.0}]}
+                  [{:date "2011-08-27"
                     :desc "Advice dogs"
                     :certain false
                     :transfers [{:account "Assets:Checking:Nordea"
@@ -22,16 +31,7 @@
                     :transfers [{:account "Assets:Checking:Nordea"
                                  :amount -200.00}
                                 {:account "Expenses:Goats"
-                                 :amount 200.0}]}]
-                  {:date "2011-08-26"
-                   :desc "Cat with gherkins"
-                   :certain true
-                   :transfers [{:account "Assets:Checking:Nordea"
-                                :amount -100.00}
-                               {:account "Expenses:Lolcats"
-                                :amount 80.0}
-                               {:account "Expenses:Vat"
-                                :amount 20.0}]}]]
+                                 :amount 200.0}]}]]]
     (is (= (apply-filter-file-to-csv-file
             filter-file
             csv-file
